08a5b91c920634f29f1b7ef4d86ec4a7d0be445f
Blatanikov <67374729+Blatanikov@users.noreply.github.com>
2020-07-17 00:25:41 +0200

    Update ReconocimientoRostros.py
    Actualizado para mostrar el nombre de la persona, sus coordenadas y también su estado de ánimo.

--- Ximies/Bally/V2.0/2_Software/3_RaspberryPiZeroW/ReconocimientoRostros/ReconocimientoRostros.py
+++ Ximies/Bally/V2.0/2_Software/3_RaspberryPiZeroW/ReconocimientoRostros/ReconocimientoRostros.py
@@ -5,8 +5,9 @@
 - Proyecto: Bally 2.0
 - Nombre del desarrollador: "Fernando Jurado Cote"
 - Para: Xpikuos
-- Version: 20.07.12.16.52.10
-- Descripción: Reconoce rostros a partir de los parametros de la base de datos creada en los anteriores
+- Version: 20.07.16.23.55.10
+- Descripción: Reconoce rostros y emociones a partir de los parametros de la base de datos creada en los anteriores.
+	Muestra nombre de la persona, coordenadas donde se encuentra y su estado de animo.
 - https://creativecommons.org/licenses/by-nc-sa/4.0/deed.es
 '''
 import time
@@ -15,15 +16,15 @@ import cv2 #sudo pip3 install opencv-contrib-python==4.1.0.25
 import imutils
 import os
 
-path = '/home/pi/BALLY/Reconocimiento de rostros/Data/'
-
+path = './Data/'
+emociones = ["Alegre","Sorprendido","Triste","Enfadado"]
 imagePaths = os.listdir(path)
 print('imagePaths=',imagePaths)
-
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-
+emotion_recognizer = cv2.face.LBPHFaceRecognizer_create()
 # Leyendo el modelo
 face_recognizer.read('modeloLBPHFace.xml')
+emotion_recognizer.read('emocionesLBPHFace.xml')
 
 cap = cv2.VideoCapture(0)
 time.sleep(0.5) #damos Tiempo a la cámara para que arranque
@@ -44,15 +45,19 @@ while True:
 	for (x,y,w,h) in faces:
 		rostro = auxFrame[y:y+h,x:x+w]
 		rostro = cv2.resize(rostro,(150,150),interpolation= cv2.INTER_CUBIC)
-		result = face_recognizer.predict(rostro)
+		emociondetectada = emotion_recognizer.predict(rostro)
+		persona = face_recognizer.predict(rostro)
 		#A partir de aquí descomentar todo si quereis ver en video los resultados
 		#Están comentados para ahorrar recursos y trabajar sin escritorio
 
-		#cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
+		#cv2.putText(frame,'{}'.format(emociondetectada),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
 		
-		if result[1] < 70:
-                        print(format(imagePaths[result[0]]), 'En x =', (x+(w/2)), ', y =', (y+(h/2)))
- 			#cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+		if persona[1] < 70:
+			print(imagePaths[persona[0]] + 'esta en x =', (x+(w/2)), ', y =', (y+(h/2)))
+			if emociondetectada[1] < 70:
+				print(imagePaths[persona[0]] + 'esta un poco ' + emociones[emociondetectada[0]])
+			#cv2.putText(frame,'{}'.format(emociones[emociondetectada[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+ 			#cv2.putText(frame,'{}'.format(imagePaths[persona[0]]),(x,y-25,),2,1.1,(0,255,0),1,cv2.LINE_AA)
 			#cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
 		#else:
 			#cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
